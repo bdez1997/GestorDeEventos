a83bb397c73a0831bab77dda23713d7bb78937bf
IreneCejas <ireneline25@gmail.com>
2022-01-10 13:59:03 +0100

    Creación modelos-interfaz
    Creación de la interfaz de máximo de caracteres y creación del modelo de persona

---
+++ GestionEventos/src/model/Persona.java
@@ -0,0 +1,71 @@
+package model;
+
+public class Persona {
+    private String sDni;
+    private String sNombre;
+    private String sApellido;
+    private String sUsername;
+    private String sPassword;
+    private String sCorreo;
+    private String sTelefono;
+    private String sRol;
+    private String sInformacion; 
+    
+    public String getsDni() {
+        return sDni;
+    }
+    public void setsDni(String sDni) {
+        this.sDni = sDni;
+    }
+    public String getsNombre() {
+        return sNombre;
+    }
+    public void setsNombre(String sNombre) {
+        this.sNombre = sNombre;
+    }
+    public String getsApellido() {
+        return sApellido;
+    }
+    public void setsApellido(String sApellido) {
+        this.sApellido = sApellido;
+    }
+    public String getsUsername() {
+        return sUsername;
+    }
+    public void setsUsername(String sUsername) {
+        this.sUsername = sUsername;
+    }
+    public String getsPassword() {
+        return sPassword;
+    }
+    public void setsPassword(String sPassword) {
+        this.sPassword = sPassword;
+    }
+    public String getsCorreo() {
+        return sCorreo;
+    }
+    public void setsCorreo(String sCorreo) {
+        this.sCorreo = sCorreo;
+    }
+    public String getsTelefono() {
+        return sTelefono;
+    }
+    public void setsTelefono(String sTelefono) {
+        this.sTelefono = sTelefono;
+    }
+    public String getsRol() {
+        return sRol;
+    }
+    public void setsRol(String sRol) {
+        this.sRol = sRol;
+    }
+    public String getsInformacion() {
+        return sInformacion;
+    }
+    public void setsInformacion(String sInformacion) {
+        this.sInformacion = sInformacion;
+    }
+   
+
+    
+}
