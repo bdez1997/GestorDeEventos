f3f1ef19c5569e2fb706abb4cbef101c342e40ae
Antonio José <bdez_1997@live.com>
2022-01-14 14:23:15 +0100

    sigo en casa

---
+++ GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/controller/AsistenciaAdapter.java
@@ -0,0 +1,33 @@
+package com.bermudez.gestioneventoandroid.controller;
+
+import static com.bermudez.gestioneventoandroid.controller.AsistenciaAdapter.*;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AsistenciaAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+        implements View.OnClickListener{
+    @Override
+    public void onClick(View view) {
+        
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
