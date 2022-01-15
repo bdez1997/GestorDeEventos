d70bce6184b8a3bf3ac4e1acd2506e493e6b17d2
IreneCejas <ireneline25@gmail.com>
2022-01-15 12:20:03 +0100

    RecyclerView Principal
    Creación del RecyclerView Principal de Android

--- GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/Fragments/PrincipalFragment.java
+++ GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/Fragments/PrincipalFragment.java
@@ -1,4 +1,4 @@
-package com.bermudez.gestioneventoandroid;
+package com.bermudez.gestioneventoandroid.Fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.bermudez.gestioneventoandroid.R;
 
 public class PrincipalFragment extends Fragment {
 
