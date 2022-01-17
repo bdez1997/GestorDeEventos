4160107b9daf5d634bba0e5655ba1833456efed8
IreneCejas <ireneline25@gmail.com>
2022-01-17 14:18:08 +0100

    Menú
    Arreglado menú

--- GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/MainActivity.java
+++ GestionEventoAndroid/app/src/main/java/com/bermudez/gestioneventoandroid/MainActivity.java
@@ -1,6 +1,11 @@
 package com.bermudez.gestioneventoandroid;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -8,21 +13,20 @@ import androidx.fragment.app.FragmentTransaction;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bermudez.gestioneventoandroid.controller.Store;
 import com.bermudez.gestioneventoandroid.fragments.AsistenciaFragment;
 import com.bermudez.gestioneventoandroid.models.Evento;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    Button btnTest;
-    ImageView imgUser;
-    Date fechaInit;
-    Date fechaFin;
+    DrawerLayout drawerLayout;
 
     @SuppressLint("ResourceType")
     @Override
@@ -30,6 +34,44 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                0,0);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        openFragment(new AsistenciaFragment());
+    }
+
+    private void openFragment(Fragment fragment){
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        /*
+        switch(item.getItemId()){
+            case R.id.opcion1: openFragment(new AsistenciaFragment()); break;
+            case R.id.opcion2: openFragment(new Opcion2Fragment()); break;
+            case R.id.opcion3: openFragment(new Opcion3Fragment()); break;
+            case R.id.salir: salir(); break;
+        }
+        */
+        return false;
     }
 
 /*
@@ -41,5 +83,8 @@ public class MainActivity extends AppCompatActivity  {
     }
 */
 
+    private void salir() {
+        finish();
+    }
 
 }
